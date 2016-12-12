d31b8e0c1593f17bfd29f812cef51eea053cfdc6
Lukas Martinelli <me@lukasmartinelli.ch>
2016-12-12 16:15:05 +0100

    Add first draft of styles page

---
+++ js/styles.js
@@ -0,0 +1,20 @@
+var styleUrls = {
+  'positron': 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
+  'dark-matter': 'https://openmaptiles.github.io/dark-matter-gl-style/style-cdn.json',
+  'osm-bright': 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json',
+  'klokantech-basic': 'https://openmaptiles.github.io/klokantech-basic-gl-style/style-cdn.json'
+};
+var maps = {};
+var mapContainers = document.getElementsByClassName('map-style');
+for (var i = 0; i < mapContainers.length; ++i) {
+  var elem = mapContainers[i];
+  var mapId = elem.id;
+  var styleUrl = styleUrls[mapId];
+  maps[mapId] = new mapboxgl.Map({
+      container: mapId,
+      style: styleUrl,
+      center: [8.5456, 47.3739],
+      zoom: 10
+  });
+  maps[mapId].addControl(new mapboxgl.NavigationControl());
+}
