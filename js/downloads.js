9d9d7bdc7725c220dcb4cb8de1bcf3ad96b02a54
Petr Sloup <slouppetr@gmail.com>
2016-12-15 13:27:35 +0100

    Downloads page functionality

--- js/downloads.js
+++ js/downloads.js
@@ -1,36 +1,91 @@
-var url = '/js/downloads.json';
+var url = 'https://openmaptiles.com/downloads/list.json';
 
-ajax(url, function (data) {
-  var worldHtml = '';
-  data['world'].forEach(function (dato) {
-    worldHtml += getItem(dato['filename'], dato['url'])
-  })
-  document.getElementById('tab-1').innerHTML = worldHtml;
+ajax(url, function(data) {
+  var addItem = function(container, name, url) {
+    var item = document.createElement('p');
+    item._name = name;
+    item.className = 'pad-2 downloads-item';
+    item.innerHTML = '<a href="' + url + '">' + name + '</a>' +
+                     '<a class="float-right" href="' + url + '">download</a>';
+    container.appendChild(item);
 
+    item.onclick = function(e) {
+      showDialog(url);
+      return false;
+    };
+  };
 
-  var coutriesHtml = '';
-  data['country'].forEach(function (dato) {
-    coutriesHtml += getItem(dato['filename'], dato['url'])
-  })
-  document.getElementById('tab-2').innerHTML = coutriesHtml;
+  var worldTab = document.getElementById('tab-1');
+  data.world.forEach(function(dato) {
+    addItem(worldTab, dato.name, dato.link);
+  });
 
+  var countriesTab = document.getElementById('tab-2');
+  data.country.forEach(function(dato) {
+    addItem(countriesTab, dato.country, dato.link);
+  });
 
-  var citiesHtml = '';
-  data['city'].forEach(function (dato) {
-    citiesHtml += getItem(dato['filename'], dato['url'])
-  })
-  document.getElementById('tab-3').innerHTML = citiesHtml;
+  var citiesTab = document.getElementById('tab-3');
+  data.city.forEach(function(dato) {
+    addItem(citiesTab, dato.city + ', ' + dato.country, dato.link);
+  });
 
-  function getItem(name, url) {
-    return '<p class="pad-2 downloads-item"><a href="' + url + '">' + name
-            + '</a><a class="float-right" href="' + url + '">download</a></p>'
-  }
+  var countrySearch = document.getElementById('search-2');
+  countrySearch.oninput = function(e) {
+    var token = countrySearch.value.toLowerCase();
+    [].forEach.call(countriesTab.querySelectorAll('p'), function(item) {
+      item.style.display =
+          item._name.toLowerCase().indexOf(token) != -1 ? '' : 'none';
+    });
+  };
+
+  var citySearch = document.getElementById('search-3');
+  citySearch.oninput = function(e) {
+    var token = citySearch.value.toLowerCase();
+    [].forEach.call(citiesTab.querySelectorAll('p'), function(item) {
+      item.style.display =
+          item._name.toLowerCase().indexOf(token) != -1 ? '' : 'none';
+    });
+  };
+
+  document.getElementById('downloads-loading').style.display = 'none';
 });
 
+document.getElementById('btn-1').addEventListener('click', function() {
+  location.hash = '';
+});
+
+document.getElementById('btn-2').addEventListener('click', function() {
+  location.hash = 'country';
+});
+
+document.getElementById('btn-3').addEventListener('click', function() {
+  location.hash = 'city';
+});
+
+var activeTabId = 1;
+if (location.hash.substr(1) == 'country') {
+  activeTabId = 2;
+} else if (location.hash.substr(1) == 'city') {
+  activeTabId = 3;
+}
+document.getElementById('btn-' + activeTabId).className += ' active';
+document.getElementById('tab-' + activeTabId).className += ' active';
+
+function showDialog(link) {
+  document.getElementById('conditions-dialog-veil').style.display =
+      document.getElementById('conditions-dialog').style.display =
+      link ? 'block' : 'none';
+
+  if (link) {
+    document.getElementById('conditions-dialog-download-link').href = link;
+    document.getElementById('conditions-dialog-download-input').value = link;
+  }
+}
 
 function ajax(url, callback) {
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       callback(JSON.parse(this.responseText));
     }
@@ -38,3 +93,5 @@ function ajax(url, callback) {
   xhr.open('GET', url, true);
   xhr.send();
 }
+
+new Clipboard('.copy-btn');
