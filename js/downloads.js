a75d5d404d0f9bfa79f34211a827d63d2e5b45c2
Dalibor Janák <dalibor.janak@gmail.com>
2017-01-05 14:47:17 +0100

    Remove downloads dialog for embed version

--- js/downloads.js
+++ js/downloads.js
@@ -1,65 +1,68 @@
 var url = 'https://openmaptiles.com/downloads/list.json';
 
-ajax(url, function(data) {
-  var addItem = function(container, name, url) {
+ajax(url, function (data) {
+  var addItem = function (container, name, url) {
     var item = document.createElement('p');
     item._name = name;
     item.className = 'pad-2 downloads-item';
     item.innerHTML = '<a href="' + url + '">' + name + '</a>' +
-                     '<a class="float-right" href="' + url + '">download</a>';
+            '<a class="float-right" href="' + url + '">download</a>';
     container.appendChild(item);
 
-    item.onclick = function(e) {
-      showDialog(url);
-      return false;
-    };
+    if (location.href.indexOf('embed') == -1) {
+      console.log('ssss')
+      item.onclick = function (e) {
+        showDialog(url);
+        return false;
+      };
+    }
   };
-
+  
   var worldTab = document.getElementById('tab-1');
-  data.world.forEach(function(dato) {
+  data.world.forEach(function (dato) {
     addItem(worldTab, dato.name, dato.link);
   });
 
   var countriesTab = document.getElementById('tab-2');
-  data.country.forEach(function(dato) {
+  data.country.forEach(function (dato) {
     addItem(countriesTab, dato.country, dato.link);
   });
 
   var citiesTab = document.getElementById('tab-3');
-  data.city.forEach(function(dato) {
+  data.city.forEach(function (dato) {
     addItem(citiesTab, dato.city + ', ' + dato.country, dato.link);
   });
 
   var countrySearch = document.getElementById('search-2');
-  countrySearch.oninput = function(e) {
+  countrySearch.oninput = function (e) {
     var token = countrySearch.value.toLowerCase();
-    [].forEach.call(countriesTab.querySelectorAll('p'), function(item) {
+    [].forEach.call(countriesTab.querySelectorAll('p'), function (item) {
       item.style.display =
-          item._name.toLowerCase().indexOf(token) != -1 ? '' : 'none';
+              item._name.toLowerCase().indexOf(token) != -1 ? '' : 'none';
     });
   };
 
   var citySearch = document.getElementById('search-3');
-  citySearch.oninput = function(e) {
+  citySearch.oninput = function (e) {
     var token = citySearch.value.toLowerCase();
-    [].forEach.call(citiesTab.querySelectorAll('p'), function(item) {
+    [].forEach.call(citiesTab.querySelectorAll('p'), function (item) {
       item.style.display =
-          item._name.toLowerCase().indexOf(token) != -1 ? '' : 'none';
+              item._name.toLowerCase().indexOf(token) != -1 ? '' : 'none';
     });
   };
 
   document.getElementById('downloads-loading').style.display = 'none';
 });
 
-document.getElementById('btn-1').addEventListener('click', function() {
+document.getElementById('btn-1').addEventListener('click', function () {
   location.hash = '';
 });
 
-document.getElementById('btn-2').addEventListener('click', function() {
+document.getElementById('btn-2').addEventListener('click', function () {
   location.hash = 'country';
 });
 
-document.getElementById('btn-3').addEventListener('click', function() {
+document.getElementById('btn-3').addEventListener('click', function () {
   location.hash = 'city';
 });
 
@@ -74,8 +77,8 @@ document.getElementById('tab-' + activeTabId).className += ' active';
 
 function showDialog(link) {
   document.getElementById('conditions-dialog-veil').style.display =
-      document.getElementById('conditions-dialog').style.display =
-      link ? 'block' : 'none';
+          document.getElementById('conditions-dialog').style.display =
+          link ? 'block' : 'none';
 
   if (link) {
     document.getElementById('conditions-dialog-download-link').href = link;
@@ -85,7 +88,7 @@ function showDialog(link) {
 
 function ajax(url, callback) {
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       callback(JSON.parse(this.responseText));
     }
