4f7ef87c27c4669c851d9a544fbf1a214cdc9133
Dalibor Janák <dalibor.janak@gmail.com>
2017-01-10 17:31:34 +0100

    Removed dependency of http libs from /styles/

--- js/styles.js
+++ js/styles.js
@@ -42,5 +42,4 @@ for (var i = 0; i < mapContainers.length; ++i) {
       bearing: bearing
   });
   maps[mapId].addControl(new mapboxgl.NavigationControl());
-  maps[mapId].addControl(new MapboxInspect());
 }
