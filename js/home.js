da256cb528bfcb18bd540fcec95aeb5d7ac86238
Petr Sloup <slouppetr@gmail.com>
2017-03-20 12:18:05 +0100

    Optimize front page performance + version without webgl (close #9)

--- js/home.js
+++ js/home.js
@@ -3,48 +3,45 @@ var center = [
   [-73.997291, 40.725438, 13],
   [2.294433, 48.858288, 14.5],
   [8.541147, 47.376333, 12]
-];
+][Math.floor(Math.random() * 4)];
 
-var rnd = Math.floor(Math.random()*4);
+if (mapboxgl.supported()) {
+  var maps = {
+    'klokantech-basic': 'https://openmaptiles.github.io/klokantech-basic-gl-style/style-cdn.json',
+    'dark-matter': 'https://openmaptiles.github.io/dark-matter-gl-style/style-cdn.json',
+    'positron': 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json',
+    'bright': 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json'
+  };
 
-function initMap(container, style) {
-  return new mapboxgl.Map({
+  var activeId = 'klokantech-basic';
+  var map = new mapboxgl.Map({
     attributionControl: false,
-    container: container,
-    style: style,
-    center: [center[rnd][0], center[rnd][1]],
-    zoom: center[rnd][2]
+    container: 'map',
+    style: maps[activeId],
+    center: [center[0], center[1]],
+    zoom: center[2]
   });
-}
 
-var maps = {
-  'klokantech-basic': initMap('map-klokantech-basic', 'https://openmaptiles.github.io/klokantech-basic-gl-style/style-cdn.json'),
-  'dark-matter': initMap('map-dark-matter', 'https://openmaptiles.github.io/dark-matter-gl-style/style-cdn.json'),
-  'positron': initMap('map-positron', 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json'),
-  'bright': initMap('map-bright', 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json')
-};
-
-function getPos(map) {
-  return [map.getCenter().lng, map.getCenter().lat, map.getZoom()]
-}
-
-function setPos(map, pos) {
-  map.setCenter([pos[0], pos[1]])
-  map.setZoom(pos[2])
-}
-
-var activeId = 'klokantech-basic';
-function switchMap(id) {
-  var oldPos = getPos(maps[activeId]);
-  var active = document.querySelector('.map.active');
-  if (active) {
-    active.className = 'map';
+  var mapEl = document.getElementById('map');
+  function switchMap(id) {
+    mapEl.className = 'map';
+    setTimeout(function() {
+      activeId = id;
+      map.setStyle(maps[activeId]);
+      document.querySelector('#navbar-top').className = id;
+      setTimeout(function() {
+        mapEl.className = 'map active';
+      }, 500);
+    }, 400);
   }
-  var newActive = document.getElementById('map-' + id);
-  newActive.className = 'map active';
-  activeId = id;
-  setPos(maps[activeId], oldPos);
-  document.querySelector('#navbar-top').className = id;
+} else {
+  L.mapbox.map(
+    'map',
+    'http://klokantech.tilehosting.com/styles/basic/rendered.json?key=tXme5cuqgrCqdPoZHqyn',
+    {
+      attributionControl: false
+    }).setView([center[1], center[0]], center[2] + 1);
+  document.querySelector('.map-switchers').style.display = 'none';
 }
 
 document.getElementById('phone').addEventListener('click', function () {
